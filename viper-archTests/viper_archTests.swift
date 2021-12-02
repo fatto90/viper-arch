c5e44dac9d4b74a211b46fbacabe76e9bd3bd58c
fattorim <fattorimatteo90@gmail.com>
2021-12-02 22:40:55 +0100

    create project

---
+++ viper-archTests/viper_archTests.swift
@@ -0,0 +1,33 @@
+//
+//  viper_archTests.swift
+//  viper-archTests
+//
+//  Created by Matteo Fattori on 02/12/21.
+//
+
+import XCTest
+@testable import viper_arch
+
+class viper_archTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
